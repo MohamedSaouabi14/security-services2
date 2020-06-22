2059c6cc06f6d0245cfcda124a4ae3552be6501a
MohamedSaouabi14 <mohamedsaouabi7@gmail.com>
2020-06-22 22:07:33 +0100

    clean code v1

--- src/main/java/org/sid/dao/AppUserRepository.java
+++ src/main/java/org/sid/dao/AppUserRepository.java
@@ -2,9 +2,12 @@ package org.sid.dao;
 
 import org.sid.entities.AppUser;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
 
 @RepositoryRestResource
 public interface AppUserRepository extends JpaRepository<AppUser, Long> {
-    AppUser findByUsername(String username);
+    @RestResource(path = "/UserByName")
+    AppUser findByUsername(@Param("cm") String username);
 }
